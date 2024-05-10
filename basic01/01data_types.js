40150ce692d2d0421794b298cc51364ed2cf7983
TAFAJJUL9923 <tafajjulkhan07860@gmail.com>
2024-05-10 04:49:57 +0000

    i have done

---
+++ basic01/01data_types.js
@@ -0,0 +1,11 @@
+let name="tafajjul"
+let age=23
+let isLoggedIn=false
+
+// number => 2 to power53
+// Bigint
+//string => ""
+// Boolean => true/false
+// null => standLONE value 
+// undefined=>
+// symbol
---
+++ basic01/variable.js
@@ -0,0 +1,19 @@
+const accountId = 1233
+let accountEmail = "akkiprine216@gmail.com"
+var accountPassword = "1234"
+let accountstate;
+
+
+
+
+    accountCity="haryana"
+    accountEmail="tafajjulkahnu64864@gamil.com"
+    accountCity="panjab"
+
+    console.table([ accountId, accountEmail, accountPassword,accountCity ,accountstate])
+
+
+
+
+// accountId = 23
+console.log(accountId);
